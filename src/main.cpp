3ca2fd7d67942f8c9e66cfef9bd527028c4ce76d
Stephane BARTHELEMY <stephane@sbarthelemy.com>
2017-02-23 04:44:26 +0100

    add arduino nano board

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 /*
-Serial to parallel on teensy 
+Serial to parallel on teensy
 */
 #include "Arduino.h"
+#ifdef BOARD_TEENSY
 #include <usb_serial.h>
+#endif
 
 unsigned long time ;
-char buffer [255];  
+char buffer [255];
 
 void usage ()
 {
@@ -21,52 +23,52 @@ void usage ()
 
 void manage_buffer ()
 {
-	unsigned char value = 0 ; 
+	unsigned char value = 0 ;
 	boolean ok = false ;
-//	Serial.println ("buffer : ");   
-//	Serial.println (buffer);   
+//	Serial.println ("buffer : ");
+//	Serial.println (buffer);
 	if ((buffer[0] >= '0') && (buffer[0] <= '9'))
 	{
-//		Serial.println ("Decimal : ");  
-		ok = true; 
-		for ( unsigned int i = 0 ; i < strlen (buffer ) ; i ++ ) 
+//		Serial.println ("Decimal : ");
+		ok = true;
+		for ( unsigned int i = 0 ; i < strlen (buffer ) ; i ++ )
 		{
-//			Serial.print ("i         : ");Serial.println (i);  
-//			Serial.print ("ok        : ");Serial.println (ok);   
-//			Serial.print ("buffer[i] : " );Serial.println (buffer[i],DEC);   
-//			Serial.print ("value     : ");Serial.println (value);   
+//			Serial.print ("i         : ");Serial.println (i);
+//			Serial.print ("ok        : ");Serial.println (ok);
+//			Serial.print ("buffer[i] : " );Serial.println (buffer[i],DEC);
+//			Serial.print ("value     : ");Serial.println (value);
 			if ((buffer[i] >= '0') && (buffer[i] <= '9')&& (ok == true))
 			{
-				ok = true; 
-				value = ( value *10 ) + (buffer[i] - '0') ; 
+				ok = true;
+				value = ( value *10 ) + (buffer[i] - '0') ;
 			}
 			else
 			{
 				ok = false ;
 			}
 		}
-		
+
 	} else if ((buffer[0] == 'X') || (buffer[0] == 'x'))
 	{
-		ok = true; 
-		for ( unsigned int i = 1 ; i < strlen (buffer ) ; i ++ ) 
+		ok = true;
+		for ( unsigned int i = 1 ; i < strlen (buffer ) ; i ++ )
 		{
 
-//			Serial.print ("i         : ");Serial.println (i);  
-//			Serial.print ("ok        : ");Serial.println (ok);   
-//			Serial.print ("buffer[i] : " );Serial.println (buffer[i],DEC);   
-//			Serial.print ("value     : ");Serial.println (value);   
+//			Serial.print ("i         : ");Serial.println (i);
+//			Serial.print ("ok        : ");Serial.println (ok);
+//			Serial.print ("buffer[i] : " );Serial.println (buffer[i],DEC);
+//			Serial.print ("value     : ");Serial.println (value);
 			if ((buffer[i] >= '0') && (buffer[i] <= '9')&& (ok == true))
 			{
-				value = ( value *16 ) + (buffer[i] - '0') ; 
+				value = ( value *16 ) + (buffer[i] - '0') ;
 			}
 			else if ((buffer[i] >= 'a') && (buffer[i] <= 'f')&& (ok == true))
 			{
-				value = ( value *16 ) + (buffer[i] - 'a' + 10) ; 
+				value = ( value *16 ) + (buffer[i] - 'a' + 10) ;
 			}
 			else if ((buffer[i] >= 'A') && (buffer[i] <= 'F')&& (ok == true))
 			{
-				value = ( value *16 ) + (buffer[i] - 'A' + 10) ; 
+				value = ( value *16 ) + (buffer[i] - 'A' + 10) ;
 			}
 			else
 			{
@@ -76,12 +78,12 @@ void manage_buffer ()
 		}
 	} else if ((buffer[0] == 'B') || (buffer[0] == 'b'))
 	{
-		ok = true; 
-		for ( unsigned int i = 1 ; i < strlen (buffer ) ; i ++ ) 
+		ok = true;
+		for ( unsigned int i = 1 ; i < strlen (buffer ) ; i ++ )
 		{
 			if ((buffer[i] >= '0') && (buffer[i] <= '1')&& (ok == true))
 			{
-				value = ( value *2 ) + (buffer[i] - '0') ; 
+				value = ( value *2 ) + (buffer[i] - '0') ;
 			}
 			else
 			{
@@ -90,7 +92,7 @@ void manage_buffer ()
 
 		}
 	}
-	if (ok == true ) 
+	if (ok == true )
 	{
 		Serial.print   ("Set Value : ");
 		Serial.print   ( value , DEC);
@@ -101,21 +103,21 @@ void manage_buffer ()
 
 		for (int i = 0 ; i < 8 ; i ++ )
 		{
-			if ((value & (1 << i )) != 0 ) 
+			if ((value & (1 << i )) != 0 )
 			{
-		                //Serial.print   ("Set high : "); Serial.println (i) ; 
+		                //Serial.print   ("Set high : "); Serial.println (i) ;
 				digitalWrite ( i , HIGH);
 
 			} else {
-		                //Serial.print   ("Set low : "); Serial.println (i) ; 
+		                //Serial.print   ("Set low : "); Serial.println (i) ;
 				digitalWrite ( i , LOW);
-			} 
+			}
 		}
 		digitalWrite(LED_BUILTIN,HIGH);
-		delay (500); 
+		delay (500);
 		digitalWrite(LED_BUILTIN,LOW);
 
-	} else 
+	} else
 	{
 		Serial.println ("Unknown value !");
 		usage();
@@ -125,7 +127,7 @@ void manage_buffer ()
 
 void setup ()
 {
-	for (int i = 0 ; i < 8 ; i ++ ) 
+	for (int i = 0 ; i < 8 ; i ++ )
 	{
 		pinMode(i, OUTPUT);
 		digitalWrite ( i , LOW);
@@ -134,14 +136,14 @@ void setup ()
 	time = millis ();
 
 	Serial.begin(115200);
-	strcpy (buffer , ""); 
+	strcpy (buffer , "");
 	usage ();
 }
 
 void loop ()
 {
 	if (Serial.available()) {
-		char sztmp[2] ; 
+		char sztmp[2] ;
 		sztmp[0] = Serial.read();  // will not be -1
 		Serial.print(sztmp[0]);
 		sztmp[1]= 0;
